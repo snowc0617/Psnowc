2f5519b6e8a5cfb24cd9bf4fc03ee641451fa033
snowc <minecraft58128@gmail.com>
2025-12-20 21:11:37 +0800

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
+  site: 'https://p.snowcnews.com',
+
   // 確保這裡是用 server 或 hybrid，看你的需求
-  output: 'server', 
-  
+  output: 'server',
+
   adapter: cloudflare({
     // ✅ 加上這一行來解決 Sharp 的警告
     imageService: 'compile', 
   }),
+
+  integrations: [sitemap()],
 });
